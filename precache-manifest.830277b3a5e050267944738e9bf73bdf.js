35490298b5cfad100e01c5166080baddf6ad058e
christeach <christian.thelen@flashtalking.com>
2019-10-17 10:09:07 +0200

    Updates

---
+++ precache-manifest.830277b3a5e050267944738e9bf73bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a42ebc91ae1f174291af3c05e00c16",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "bd5fe014b71b34e55658",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6b46a1752e9297e1a662",
+    "url": "/robofriends/static/css/main.bcd8154d.chunk.css"
+  },
+  {
+    "revision": "bd5fe014b71b34e55658",
+    "url": "/robofriends/static/js/2.70a12630.chunk.js"
+  },
+  {
+    "revision": "6b46a1752e9297e1a662",
+    "url": "/robofriends/static/js/main.d0c20ccf.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
